826f11896914ccf827f7957be04e20dacdbfe4fc
Ograeset <tedros963@gmail.com>
2022-11-15 14:28:10 +0100

    Initial commit

---
+++ src/main/java/com/nosa/orso/OrsoApplication.java
@@ -0,0 +1,13 @@
+package com.nosa.orso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nosa/orso/OrsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosa.orso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
