3c972c35e4635771cfb78028e6f682cd6ac0caf7
Sally Resch <sallyresch27@gmail.com>
2022-11-23 11:59:41 +0100

    create a customer in orsoapplication. paircoding/Sally

--- src/main/java/com/nosa/orso/OrsoApplication.java
+++ src/main/java/com/nosa/orso/OrsoApplication.java
@@ -1,13 +1,37 @@
 package com.nosa.orso;
 
+import com.nosa.orso.customer.Allergies;
+import com.nosa.orso.customer.Customer;
+import com.nosa.orso.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class OrsoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(OrsoApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner runner (CustomerRepository customerRepository, MongoTemplate mongoTemplate){
+        return args -> {
+            Allergies allergies = new Allergies(
+                    true,
+                    false,
+                    false,
+                    false);
+            
+            Customer customer = new Customer(
+                    "Test Testsson",
+                    "test@mail.com",
+                    4,
+                    true,
+                    allergies
+            );
+        };
+    }
 }
